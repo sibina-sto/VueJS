07749e6f866fe7dca27cff996123f14bc6a04d55
Sibina Stoyanova <stoyanovasibina@gmail.com>
2023-11-08 14:08:29 +0200

    Add files via upload

---
+++ 04-workshop-components/Doc/04-components-workshop_resources/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import ComponentsStarter from './components/ComponentsStarter.vue';
+
+export default {
+  components: {
+    ComponentsStarter,
+  },
+};
+</script>
+
+<template>
+  <ComponentsStarter />
+</template>
