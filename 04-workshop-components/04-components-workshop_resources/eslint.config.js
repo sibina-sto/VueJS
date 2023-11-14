bea93c7a80dfc4bdc81c11436f375e91260ee445
Sibina Stoyanova <stoyanovasibina@gmail.com>
2023-11-14 19:40:55 +0200

    Update eslint.config.js

--- 04-workshop-components/04-components-workshop_resources/eslint.config.js
+++ 04-workshop-components/04-components-workshop_resources/eslint.config.js
@@ -4,8 +4,23 @@ export default antfu({
   vue: true,
   typescript: false,
 }, {
+  files: ['**/*.vue'],
   rules: {
+    'vue/singleline-html-element-content-newline': 'error',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
+}, {
+  rules: {
+    'curly': 'off',
     'no-console': 'off',
     'style/semi': ['error', 'always'],
+    'ts/consistent-type-definitions': ['error', 'type'],
   },
 });
